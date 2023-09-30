9680d236191463ceb8359db299bd5f4bff885287
ProfScrew <maksym.nov@gmail.com>
2023-09-30 21:01:46 +0200

    Angular with bootstrap
    Co-authored-by: Stefano Masiero <St3f4n0Masi3r0@users.noreply.github.com>

--- webserver/frontend/angular/README.md
+++ webserver/frontend/angular/README.md
@@ -1,6 +1,6 @@
 # Angular
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.4.
 
 ## Development server
 
@@ -25,24 +25,3 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
-
-
-
-
-# Ingredient
-    - id
-    - name
-    - cost_per_unit
-    - type
-
-# Dish
-    - id
-    - name
-    - ingredients[]
-    - price
-    - type
-
-# SubMenu
-    - id
-    - name
-    - base_color
--- webserver/frontend/angular/src/app/app.module.ts
+++ webserver/frontend/angular/src/app/app.module.ts
@@ -3,64 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
-import { HTTPErrorComponent } from './pages/httperror/httperror.component';
-import { WaiterTablesComponent } from './pages/waiter/tables/tables.component';
-import { TableComponent } from './components/table/table.component';
-import { ModalComponent } from './components/modal/modal.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { WaiterMenuComponent } from './pages/waiter/menu/menu.component';
-import { ButtonComponent } from './components/button/button.component';
-import { CheckboxComponent } from './components/checkbox/checkbox.component';
-import { HttpClientModule } from '@angular/common/http';
-import { AuthService } from './services/auth.service';
-import { MenuItemComponent } from './pages/waiter/menu/menu.item.component';
-import { OrderComponent } from './pages/order/order.component';
-import { CourseComponent } from './components/order/course/course.component';
-import { DishComponent } from './components/order/dish/dish.component';
-import { ListComponent } from './components/order/list/list.component';
-import { InputComponent } from './components/input/input.component';
-import { UserInteractionComponent } from './components/interaction/interaction.component';
-import { MenuViewComponent } from './components/menu/view/view.component';
-import { SelectComponent } from './components/select/select.component';
-import { ProdComponent } from './pages/prod/prod.component';
-import { CardComponent } from './components/card/card.component';
-import { TableService } from './services/table.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    HTTPErrorComponent,
-    WaiterTablesComponent,
-    WaiterMenuComponent,
-    OrderComponent,
-    ListComponent,
-    ProdComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TableComponent,
-    ModalComponent,
-    NavigationComponent,
-    ButtonComponent,
-    CheckboxComponent,
-    MenuItemComponent,
-    CourseComponent,
-    DishComponent,
-    InputComponent,
-    UserInteractionComponent,
-    MenuItemComponent,
-    MenuViewComponent,
-    SelectComponent,
-    CardComponent,
-    HttpClientModule
+    NgbModule
   ],
-  providers: [
-    {provide: AuthService,  useClass: AuthService},
-    {provide: TableService, useClass: TableService}
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
