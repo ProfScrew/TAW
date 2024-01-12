bd02a2687ced9560255d9c5848e027df30941978
Stefano Masiero <58975318+St3f4n0Masi3r0@users.noreply.github.com>
2024-01-12 17:06:27 +0100

    Delete webserver/multi_platform_generate directory

--- webserver/multi_platform_generate/general_config/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    URL_BACKEND: 'https://studious-couscous-xp4969q6w9q3vvjp-80.app.github.dev/api/',
-    URL_STATIC: 'https://studious-couscous-xp4969q6w9q3vvjp-80.app.github.dev/static/',
-    // URL_BACKEND: 'http://localhost:8080/',
-    VERSION: 'v1',
-    production: false
-};
