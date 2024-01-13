ff8a2540edb6fd3223fd479195fb60906adffc9b
Stefano Masiero <58975318+St3f4n0Masi3r0@users.noreply.github.com>
2024-01-13 02:37:47 +0100

    update host

--- webserver/multi_platform_generate/general_config/environment.ts
+++ webserver/multi_platform_generate/general_config/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    URL_BACKEND: 'https://friendly-winner-w54969jpwvg27r9-443.app.github.dev/api/',
-    URL_STATIC: 'https://friendly-winner-w54969jpwvg27r9-443.app.github.dev/static/',
+    URL_BACKEND: 'https://humble-yodel-w4vwpqwp573g9rg-443.app.github.dev/api/',
+    URL_STATIC: 'https://humble-yodel-w4vwpqwp573g9rg-443.app.github.dev/static/',
     // URL_BACKEND: 'http://localhost:8080/',
     VERSION: 'v1',
     production: false
--- webserver/multi_platform_generate/general_config/socket.service.ts
+++ webserver/multi_platform_generate/general_config/socket.service.ts
@@ -10,7 +10,7 @@ export class SocketService {
   private socket: any;
 
   constructor() {
-    this.socket = io("https://friendly-winner-w54969jpwvg27r9-443.app.github.dev/",{ path: '/socket/'});
+    this.socket = io("https://humble-yodel-w4vwpqwp573g9rg-443.app.github.dev/",{ path: '/socket/'});
   }
 
   listen(event: string): Observable<any> {
